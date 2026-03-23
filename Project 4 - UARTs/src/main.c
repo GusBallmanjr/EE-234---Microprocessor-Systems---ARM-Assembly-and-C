e6d51a9090e7e3a26bc29861dba3a395d70ac3e8
Gus Ballman <gus.ballman@wsu.edu>
2026-03-22 17:51:51 -0700

    Update and rename Project4.c to main.c

---
+++ Project 4 - UARTs/src/main.c
@@ -0,0 +1,14 @@
+#include "UART.h"
+//#include "Project4.h"
+char string_data[128];
+
+int main(void){
+
+	configure_UART1();
+    //UART1_getS(string_data);
+	//UART1_putC('F');
+	//UART1_putS("Hello There");
+
+
+    return 0;
+}
