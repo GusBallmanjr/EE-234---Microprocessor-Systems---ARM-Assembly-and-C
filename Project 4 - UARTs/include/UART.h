67c45c256c6d65dec8d4ab344e31f4f5963cc4e4
Gus Ballman <gus.ballman@wsu.edu>
2026-03-31 09:54:03 -0700

    Update UART.h

--- Project 4 - UARTs/include/UART.h
+++ Project 4 - UARTs/include/UART.h
@@ -1,19 +1,30 @@
-#ifndef UART_H
-#define UART_H
-
+#ifndef UART1_H
+#define UART1_H
 #include <stdint.h>
 
-#define UART1_BASE 0xE0001000
 #define BAUD_DIV 6
 #define BAUDGEN 0x7C
 
+typedef struct UART1 {
+    volatile uint32_t* BASE;
+    volatile uint32_t* STATUS;
+    volatile uint32_t* IMR;
+    volatile uint32_t* IER;
+    volatile uint32_t* IDR;
+    volatile uint32_t* ISR;
+    volatile uint32_t* RXTG;
+} UART1;
+
+extern UART1 U1;
+
 void UART1_reset(void);
-void configure_UART1();
+void configure_UART1(void);
 uint32_t UART1_empty_TX(void);
 uint32_t UART1_empty_RX(void);
 char UART1_getC(void);
 void UART1_putC(char data);
-void UART1_putS(char buffer[]);
+void UART1_putS(char str[]);
 uint32_t UART1_getln(char str[], uint32_t max);
+void UART1_sendInt(uint8_t integer);
 
 #endif
